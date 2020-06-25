e78d28b569213bac84abc62d41d95d572281951c
Alexis Guyot <51441206+alex0267@users.noreply.github.com>
2020-06-25 17:11:29 +0200

    Create deploy.sh

---
+++ deploy-ml-model-with-AppEngine/application/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install -r requirements.txt -t lib
+gcloud app create
+gcloud app deploy
+
+PROJECT=$(gcloud config get-value project)
+echo "Visit https://PROJECT-ID.appspot.com/  e.g. https://${PROJECT}.appspot.com"
